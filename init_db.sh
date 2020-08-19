009b35e5f76a27cdb3963d29b514b5a079582867
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-08-19 18:20:44 +0100

    Add database create script

---
+++ init_db.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# exit immediately on error
+set -e
+
+GREEN="\033[0;92m"
+RESET="\033[0m"
+
+psql -q -c "CREATE USER fac_resources_user SUPERUSER PASSWORD 'local123'"
+echo "${GREEN}Postgres user 'fac_resources_user' created${RESET}"
+
+psql -q -c "CREATE DATABASE fac_resources WITH OWNER fac_resources_user"
+echo "${GREEN}Postgres database 'fac_resources' created${RESET}"
+
+cp .env.example .env
+echo "${GREEN}'.env' file created${RESET}"
