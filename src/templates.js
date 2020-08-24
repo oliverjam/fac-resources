ae7cf1265a84264ae69f22bf01a3b54abd96a526
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-08-24 15:10:18 +0100

    Add bookmark action button

--- src/templates.js
+++ src/templates.js
@@ -29,13 +29,13 @@ const ResourceList = ({ resources }) => html`
       const votes = r.total_votes;
       return html`
         <li class="hstack" style="--pad: 1vw; --radius: var(--size-sm)">
-          <img src="${favicon}" width="32" height="32" alt="" />
-          <div class="vstack" style="--gap: var(--size-sm)">
+          <img src="${favicon}" width="36" height="36" alt="" />
+          <div class="vstack" style="--gap: var(--size-md)">
             <div class="vstack block-link"" style="--gap: 0">
               <h3>${r.title}</h3>
               <a href="${r.url}">${r.url}</a>
             </div>
-            ${Votes({ votes })}
+            ${Actions({ votes, name: r.title })}
           </div>
         </li>
       `;
@@ -43,17 +43,46 @@ const ResourceList = ({ resources }) => html`
   </ul>
 `;
 
-const Votes = ({ votes }) => {
+const Actions = ({ votes, name }) => {
   return html`
-    <div class="hstack" style="--gap: var(--size-xs)">
-      <svg viewBox="0 0 20 20" width="32" height="32" fill="currentColor">
-        <path
-          fill-rule="evenodd"
-          d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-          clip-rule="evenodd"
-        ></path>
-      </svg>
-      <strong>${votes} ${votes === 1 ? "vote" : "votes"}</strong>
+    <div class="hstack" style="--gap: var(--size-lg)">
+      <div class="hstack" style="--gap: var(--size-xs)">
+        <button class="icon" aria-label="Vote for ${name}" style="--color: red">
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            width="32"
+            height="32"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            ></path>
+          </svg>
+        </button>
+        <strong>${votes}</strong>
+      </div>
+      <div class="hstack" style="--gap: var(--size-xs)">
+        <button class="icon" aria-label="Bookmark ${name}">
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            width="32"
+            height="32"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"
+            ></path>
+          </svg>
+        </button>
+      </div>
     </div>
   `;
 };
